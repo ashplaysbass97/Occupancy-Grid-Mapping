d06d98e88e143e85813dbf5d170ba25cebad393a
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-10-08 12:33:25 +0100

    Set up the ultrasonic sensor
    Set up the ultrasonic sensor and whether the bumpers have been pressed and distance from the ultrasonic sensor are now displayed on the LCD screen.

--- src/SmartRobot.java
+++ src/SmartRobot.java
@@ -1,26 +1,40 @@
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.Font;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 
 public class SmartRobot {
     private Brick ev3;
+    private GraphicsLCD lcd;
     private EV3TouchSensor leftBumper, rightBumper;
-    private SampleProvider leftBumperSampleProvider, rightBumperSampleProvider;
-    private float[] leftBumperSample, rightBumperSample;
+	private EV3UltrasonicSensor ultrasonicSensor;
+    private SampleProvider leftBumperSampleProvider, rightBumperSampleProvider, ultrasonicSampleProvider;
+    private float[] leftBumperSample, rightBumperSample, ultrasonicSample;
+    private NXTRegulatedMotor ultrasonicSensorMotor;
 
     public static void main(String[] args) {
         SmartRobot myRobot = new SmartRobot();
         
-        // test the bumpers
+        // display whether the bumpers have been pressed and the distance from the ultrasonic sensor
         while (!Button.ESCAPE.isDown()) {
-        	if (myRobot.isLeftBumperPressed()) {
-            	System.out.println("Left bumper");
-            }
-            if (myRobot.isRightBumperPressed()) {
-            	System.out.println("Right bumper");
-            }        	
+        	myRobot.lcd.clear();
+            myRobot.lcd.setFont(Font.getSmallFont());
+        	myRobot.lcd.drawString("LBump: " + myRobot.isLeftBumperPressed(), 0, 20, 0);
+        	myRobot.lcd.drawString("RBump: " + myRobot.isRightBumperPressed(), 0, 30, 0);
+        	myRobot.lcd.drawString("Dist: " + myRobot.getDistance(), 0, 40, 0); 
+    		try {
+				Thread.sleep(400);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
         
         myRobot.closeRobot();
@@ -29,7 +43,9 @@ public class SmartRobot {
     // SmartRobot constructor
     public SmartRobot() {
         ev3 = BrickFinder.getDefault();
+        lcd = LocalEV3.get().getGraphicsLCD();
         setupTouchSensor();
+        setupUltrasonicSensor();
     }
 
     // set up the bumpers
@@ -40,6 +56,14 @@ public class SmartRobot {
         rightBumperSampleProvider = rightBumper.getTouchMode();
         leftBumperSample = new float[leftBumperSampleProvider.sampleSize()];
         rightBumperSample = new float[rightBumperSampleProvider.sampleSize()];
+    }
+    
+	// set up the ultrasonic sensor
+    private void setupUltrasonicSensor() {
+        ultrasonicSensorMotor = Motor.C;
+        ultrasonicSensor = new EV3UltrasonicSensor(ev3.getPort("S3"));
+        ultrasonicSampleProvider = ultrasonicSensor.getDistanceMode();
+        ultrasonicSample = new float[ultrasonicSampleProvider.sampleSize()];
     }
 
     // whether the left bumper is pressed
@@ -53,6 +77,12 @@ public class SmartRobot {
         rightBumperSampleProvider.fetchSample(rightBumperSample, 0);
         return (rightBumperSample[0] == 1.0);
     }
+    
+    // get the distance from the ultrasonic sensor
+    public float getDistance() {
+    	ultrasonicSampleProvider.fetchSample(ultrasonicSample, 0);
+    	return ultrasonicSample[0];
+	}
 
     // close the bumpers
     private void closeRobot() {
