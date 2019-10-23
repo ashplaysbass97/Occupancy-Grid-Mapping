e3dfd2201cc8889f10843c0fc05af2bfec482b21
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-10-23 11:31:31 +0100

    Updated PilotMonitor to display robot's position and occupied cells

--- src/monitors/PilotMonitor.java
+++ src/monitors/PilotMonitor.java
@@ -22,11 +22,10 @@ public class PilotMonitor extends Thread {
 	}
 
 	public void run() {
-        while (running) {
-        	lcd.clear();
-            lcd.setFont(Font.getSmallFont());
-            cellCounter = 0;
-            updateMap();
+		while (running) {
+			lcd.clear();
+			lcd.setFont(Font.getSmallFont());
+			updateMap();
 
     		try {
 				sleep(500);
@@ -34,11 +33,10 @@ public class PilotMonitor extends Thread {
 				e.printStackTrace();
 				running = false;
 			}
-        }
+		}
 	}
 
 	/**
-	 * WIP
 	 * Draws the 6 by 7 Occupancy grid used to map the environment.
 	 */
 	public final void updateMap() {
@@ -49,19 +47,27 @@ public class PilotMonitor extends Thread {
 			String rowString = "|";
 			
 			for (int x = 0; x < grid.getGridWidth(); x++) {
-					double probability = grid.findUsingCoordinate(x, y).getOccupancyProbability();
+				double probability = grid.findUsingCoordinate(x, y).getOccupancyProbability();
 			
-					// check if probability is unknown
-					if (probability == -1) {
-						// add empty characters before and after the ? symbol so it centers in lcd grid cell.
-						rowString += " ? ";
-					} else {
-						// only display 1 significant digit and since probaility does not go greater than 1 we can assume this will be 3 characters.
-						BigDecimal bd = new BigDecimal(probability);
-						bd = bd.round(new MathContext(3));
-						rowString += bd.doubleValue();
-					}
-					rowString += "|";
+				// display the robot's current position
+				if (grid.getCurrentCell() == grid.findUsingCoordinate(x, y)) {
+					rowString += " R ";
+					
+				// display if the cell is unknown
+				} else if (probability == -1) {
+					rowString += " ? ";
+					
+				// display if the cell is occupied
+				} else if (probability == 1) {
+					rowString += "|||";
+					
+				// display occupancy probability to 1 significant digit
+				} else {
+					BigDecimal bd = new BigDecimal(probability);
+					bd = bd.round(new MathContext(3));
+					rowString += bd.doubleValue();
+				}
+				rowString += "|";
 			}
 			
 			lcd.drawString(rowString, 0, (rowCounter) * 10, 0);
